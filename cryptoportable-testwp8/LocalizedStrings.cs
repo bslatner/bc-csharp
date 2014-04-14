50c43274af98824feb9c4ab21e149a664314ae07
Bryan Slatner <bsgit@slatner.com>
2014-04-14 00:33:54 -0400

    Added Windows Phone 8 test project

---
+++ cryptoportable-testwp8/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using crypto.test.Resources;
+
+namespace crypto.test
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
